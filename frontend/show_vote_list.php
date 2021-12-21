8ed045adc4f0c7fc6043d80164097d784b928bc2
yo76317 <yo76317@gmail.com>
2021-12-22 03:05:29 +0800

    首頁投票頁美化 1.加上列表。 2.列表右邊加上膠囊顯示投票總數。 3.列表右邊加上觀看結果按紐。

--- frontend/show_vote_list.php
+++ frontend/show_vote_list.php
@@ -4,21 +4,33 @@
 在選項上加上超聯結並給值?do=vote&id=
 加上資料庫關聯id,連到顯示頁後用get使用id -->
 <?php
-$alltopics=all('topics');
-echo "<ol>";
-foreach ($alltopics as $key => $value) {
-    // 找到options資料表topicid筆數 => 0 就執行以下
-    // 陣列topic欄位數值，li會加排序數字
-    // 聯結加上topic的變數id
-    if(rows('options',['topic_id'=>$value['id']]) > 0){
-    echo "<li>";    
-    echo "<a href='index.php?do=vote&id={$value['id']}'>";
-    echo $value['topic'];
-    echo "</a>";
-    echo "</li>";
-    // dd($value);
-    }
-    }
-echo "</ol>";
+    $alltopics=all('topics');
+        echo "<ol class='list-group'>";
+        foreach ($alltopics as $key => $value) {
+        // 找到options資料表topicid筆數 => 0 就執行以下
+        // 陣列topic欄位數值，li會加排序數字
+        // 聯結加上topic的變數id
+        if(rows('options',['topic_id'=>$value['id']]) > 0){
+        echo "<li class='list-group-item'>";
+        // 列出題目
+        echo "<a class='d-inline-block col-md-8' href='index.php?do=vote&id={$value['id']}'>";
+        echo $value['topic'];
+        echo "</a>";
+        
+        // 總投票數膠囊顯示
+        $count=q("select sum(`count`) as '總計' from `options` where `topic_id`='{$value['id']}'");
+        echo "<span class='badge badge-pill badge-warning'>";
+        echo $count[0]['總計'];
+        echo "</span>";
 
+        //看結果按鈕
+        echo "<span class='col-2 float-right text-center'>";
+        echo "<button type='button' class='btn btn-outline-warning'>觀看結果</button>";
+        echo "</span>";
+
+        echo "</li>";
+            // dd($value);
+            }
+            }
+        echo "</ol>";
 ?>
